644f9e47be900fa5a9ea3f0469d742bb5f4fa1d1
lawrence segun <lawrencesegun025@gmail.com>
2023-11-06 13:12:26 +0100

    ui restructure

--- src/components/DayContent.jsx
+++ src/components/DayContent.jsx
@@ -10,7 +10,7 @@ const DayContent = () => {
     const {id} = useParams()
     const {day_number} = useParams()
     const [lobbies, setLobbies] = useState([])
-    const [lobbyNumber, setLobbyNumber] = useState(0)
+    const [lobbyNumber, setLobbyNumber] = useState("")
     const [showForm, setShowForm] = useState(false)
     const [players, setPlayers] = useState([])
     const [playerID, setPlayerID] = useState("")
@@ -37,6 +37,9 @@ const DayContent = () => {
       if( playerID === ""){
         return
       }
+      if(playerID == "_") {
+        return
+      }
       const data = await getRequest(PlayerKillsInADay(id, day_number, playerID))
       setTotalKillsData(data)
     }
@@ -60,6 +63,7 @@ const DayContent = () => {
     useEffect(() => {
       if(isSuccess) {
         queryClient.invalidateQueries({ queryKey: ['Lobbies'] })
+        setLobbyNumber("")
       }
       // eslint-disable-next-line
     }, [isSuccess])
@@ -72,10 +76,10 @@ const DayContent = () => {
         isError ? <div className=' bg-red-500 rounded-sm p-3 text-white text-2xl text-center'>{err?.response?.data.response}</div> 
         : !loading ?
           <div className='container grid grid-cols-1 md:grid-cols-2 md:p-5'>
-            <div className=' container mx-auto px-4 grid grid-cols-1 ' >
+            <div className=' container mx-left px-4 grid grid-cols-1 ' >
             {
-              lobbies?.length == 0 ? 
-              <div className=' text-green-500 bg-gray-100 pl-1 py-5 text-2xl font-bold'>There's no player in this lobby yet</div>
+              lobbies?.length === 0 ? 
+              <div className=' text-green-500 bg-gray-100 pl-1 py-5 text-2xl font-bold'>No Lobby has been created today</div>
               :
                lobbies?.map((lobby, i) => {
                 return (
@@ -85,6 +89,27 @@ const DayContent = () => {
                 )
               })
             }
+            <div className='mx-left my-5'>
+        {
+          lobbies?.length === 3 ? <div className=' text-green-500 bg-gray-100 pl-1 py-5 text-2xl font-bold'>Lobby Is Filled!</div> :
+
+         <button onClick={controlShowForm} className=' bg-lime-500 text-white px-5 py-2 '>Create Lobby</button>
+        }
+        {
+          lobbies?.length !== 3 ?
+          showForm ? 
+          <form method="post" className='my-3 flex flex-col  justify-start align-top' onSubmit={createLobby}>
+            {isLobbyCreateErr ? <div className=' bg-red-500 rounded-sm p-3 text-white text-2xl text-center'>{lobbyCreateErr?.response?.data.response}</div> : ""}
+            <div className='h-full'> 
+              <label htmlFor="lobby_number" className='sr-only'>Lobby Number</label>
+              <input type="number" onChange={e => setLobbyNumber(e.target.value)} min={0} max={3} className='border border-gray-300 rounded-sm h-[100%] p-3 min-h-[50px]' value={lobbyNumber} placeholder='Lobby Number e.g 1' />
+            </div>
+            
+            <button className=' bg-green-400 py-3 w-[100px] mt-3'>Submit</button>
+          </form> : "" : ""
+        }
+        
+      </div>
             </div>
             <div >
               <form className='flex flex-col gap-4 ml-4' onSubmit={getTotalPlayerKillsInADay} method='GET'>
@@ -98,8 +123,10 @@ const DayContent = () => {
                     onChange={e => setPlayerID(e.target.value)}
                     onClick={getPlayersInDay}
                     >
-                      <option value="">Get Player Total Kills </option>
+                      <option value="">Select Player</option>
                       {
+                        players === null ? <option value="_" disabled>No Player yet</option> :
+                      
                       players?.map((player, i) => {
                         return <option value={player.player_id} key={i}>{player.name}</option>
                       })
@@ -122,27 +149,7 @@ const DayContent = () => {
            
         : <div className=' bg-yellow-500 p-5'>Loading, please wait...</div>
       }
-      <div className='mx-auto px-4 my-5'>
-        {
-          lobbies?.length === 3 ? <div className=' text-green-500 bg-gray-100 pl-1 py-5 text-2xl font-bold'>Lobby Is Filled!</div> :
-
-         <button onClick={controlShowForm} className=' bg-lime-500 text-white px-5 py-2 '>Create Lobby</button>
-        }
-        {
-          lobbies?.length !== 3 ?
-          showForm ? 
-          <form method="post" className='my-3 flex flex-col  justify-start align-top' onSubmit={createLobby}>
-            {isLobbyCreateErr ? <div className=' bg-red-500 rounded-sm p-3 text-white text-2xl text-center'>{lobbyCreateErr?.response?.data.response}</div> : ""}
-            <div className='h-full'> 
-              <label htmlFor="lobby_number">Lobby Number</label>
-              <input type="number" onChange={e => setLobbyNumber(e.target.value)} min={0} max={3} className='border border-gray-300 ml-3 rounded-sm h-[50px] p-3' />
-            </div>
-            
-            <button className=' bg-green-400 py-3 w-[100px] mt-3'>Submit</button>
-          </form> : "" : ""
-        }
-        
-      </div>
+      
     </div>
   )
 }
--- src/container/ViewTournaments.jsx
+++ src/container/ViewTournaments.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react'
 import TournamentCard from '../components/TournamentCard'
-import GoBack from '../components/GoBack'
 import { useQuery } from '@tanstack/react-query'
 import { getRequest } from '../api/ApiCall'
 import { Tournaments } from '../api/APiURL'
@@ -19,7 +18,6 @@ const ViewTournaments = () => {
     useEffect(() => setTournaments(data), [data])
   return (
     <>
-        <GoBack />
         {isError ? <div className=' bg-red-500 rounded-sm p-3 text-white text-2xl text-center'>{err.message}</div> : ""}
         <div className='container mx-auto px-4 grid gap-5 grid-cols-1 md:grid-cols-2 mt-5 pb-5'>
             {
